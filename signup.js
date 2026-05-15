c46f26a33422a5d6370c02688e72fc184f79dd39
Alicia Munoz <alimulo2001@gmail.com>
2026-05-15 14:06:02 +0200

    formulario de registrarte como usuario

---
+++ signup.js
@@ -0,0 +1,16 @@
+import { signupController } from "./signup/signup-controller.js";
+import { notificationsController } from "./notifications/notification-controller.js";
+
+const signupForm = document.querySelector('form');
+const notificationsContainer = document.querySelector('.notifications-container');
+
+const { showNotification } = notificationsController(notificationsContainer)
+
+signupForm.addEventListener('userCreated', (event) => {
+  showNotification(event.detail.message, event.detail.type)
+})
+signupForm.addEventListener('userNotCreated', (event) => {
+  showNotification(event.detail.message, event.detail.type)
+})
+
+signupController(signupForm)
